0d4d134c2a92f3855c60b35d9390dc68757599d1
Galina Atamankina <g.atamankina@gmail.com>
2025-07-18 10:46:15 +0200

    PLPG-API-8: Implemented get topic by id endpoint.

--- app.py
+++ app.py
@@ -27,5 +27,13 @@ def get_skills():
     return jsonify(skills)
 
 
+@app.route('/topics/<id>', methods=['GET'])
+def get_topic_by_id(id):
+    topics = data_manager.read_data(TOPICS_FILE)
+    found_topics = [t for t in topics if t['id'] == id]
+    topic = found_topics[0] if found_topics else None
+    return jsonify(topic)
+
+
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
